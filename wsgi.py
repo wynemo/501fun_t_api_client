01b83999b6eba4d21357dbc5070b6bea6ea8c291
wynemo <ggdabin@gmail.com>
2012-04-26 06:53:59 +0000

    add submodules

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import web
+
+urls = ('/','501fun_t_api.twitterapi.ttcbk.index',
+    '/auth','501fun_t_api.twitterapi.ttauth.auth',
+    '/api/(.*)','501fun_t_api.twitterapi.ttapi.api',
+)
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
+if __name__=="__main__":
+    app.run()
+    pass
